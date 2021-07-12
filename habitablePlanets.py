1bb03ef0739bee723258928af92bde8ff6ce0c8b
alandliu <52473359+alandliu@users.noreply.github.com>
2021-07-11 18:54:51 -0700

    Add files via upload

---
+++ habitablePlanets.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+habitable_planets = np.array(["Teegarden's Star b", "TOI-700 d", "K2-72 e", "TRAPPIST-1 d", "Kepler-1649 c", 
+          "Proxima Cen b", "GJ 1061 d", "GJ 1061 c", "Ross 128 b", "GJ 273 b", "TRAPPIST-1 e", "Kepler-442 b",
+            "Wolf 1061 c", "GJ 667 C c", "GJ 667 C f", "Kepler-1229 b", "TRAPPIST-1 f", "Kepler-62 f", "Teegarden's Star c", "Kepler-186 f", "GJ 667 C e", 
+            "tau Cet f", "TRAPPIST-1 g", "GJ 682 b", "Kepler-452 b", "Kepler-62 e", "Kepler-1652 b", "Kepler-1544 b",
+            "Kepler-296 e", "Kepler-283 c", "K2-296 b", "Kepler-1410 b", "K2-3 d", "Kepler-1638 b", "Kepler-296 f",
+            "Kepler-440 b", "Kepler-705 b", "Kepler-1653 b", "GJ 832 c", "Kepler-1606 b", "Kepler-1090 b", "Kepler-61 b",
+            "K2-18 b", "Kepler-443 b", "Kepler-1701 b", "Kepler-22 b", "LHS 1140 b", "Kepler-1552 b", "K2-9 b", 
+            "Kepler-1540 b", "GJ 180 c", "Kepler-1632 b", "Kepler-298 d", "GJ 163 c", "HD 40307 g", "K2-288 B b", "GJ 3293 d", "GJ 229 A c", "Kepler-174 d", "GJ 357 d"
+          ])
